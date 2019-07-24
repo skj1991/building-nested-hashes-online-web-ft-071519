809104805c02fb5035f9ac4277a6bdec4da63b3a
Stacy Johns <stacykjohns@icloud.com>
2019-07-24 19:45:48 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,29 +2,37 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name = "",
+        :age = ""
         },
       :matriarch => {
-        #your key/value pairs here
+        :name = "",
+        :age = ""
         },
       :hero => {
-        #your key/value pairs here
+        :name = "",
+        :age = "",
+        :status = ""
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name = "",
+        :age = ""
         },
       :matriarch => {
-        #your key/value pairs here
+        :name = "",
+        :age = ""
         },
       :heroine => {
-        #your key/value pairs here
+        :name = "",
+        :age = "",
+        :status = ""
         },
       :heroine_friends => []
    }
   }
 
-  
+
 end
